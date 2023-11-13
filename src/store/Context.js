83cd3ec93f72332e6a89717b354bd3e9b88ff273
Tolesh <57959682+Tolesh@users.noreply.github.com>
2023-11-13 14:04:50 +0600

    Update Context.js

--- src/store/Context.js
+++ src/store/Context.js
@@ -2,29 +2,27 @@ import React from 'react'
 
 // import appLogo from '../images/amplichat.png'
 import appLogo from '../images/jsk.png'
-import appleStoreBadge from '../images/apple_store_badge.svg'
-import googlePlayBadge from '../images/google_play_badge.png'
 import telegramBadge from '../images/telegramButton.svg'
 
 import coverImage from '../images/amplichat_iphones_two.png'
 import doctorImage from '../images/doctor3.jpg'
 // endorsement images
-import dreamhub_filled from '../images/dreamhub_filled.png'
-import voiceqna_filled from '../images/voiceqna_fill_v2_gradient.png'
-import voicemirror_filled from '../images/voice_mirror_v_1024.png'
-import bazipaipai_unfilled from '../images/bazipaipai_64.png'
-import spindrifthome_filled from '../images/sh_unfill_1024.png'
+// import dreamhub_filled from '../images/dreamhub_filled.png'
+// import voiceqna_filled from '../images/voiceqna_fill_v2_gradient.png'
+// import voicemirror_filled from '../images/voice_mirror_v_1024.png'
+// import bazipaipai_unfilled from '../images/bazipaipai_64.png'
+// import spindrifthome_filled from '../images/sh_unfill_1024.png'
 
 
 // section images
-import guitar from '../images/guitar.jpg'
-import event_phones from '../images/event_phones.jpeg'
-import foggy_blue from '../images/foggy_blue.jpg'
-import purple_phones from '../images/purple_phones.png'
-import purple_light from '../images/purple_light.jpg'
-import concert_classic from '../images/concert_classic.jpg'
+// import guitar from '../images/guitar.jpg'
+// import event_phones from '../images/event_phones.jpeg'
+// import foggy_blue from '../images/foggy_blue.jpg'
+// import purple_phones from '../images/purple_phones.png'
+// import purple_light from '../images/purple_light.jpg'
+// import concert_classic from '../images/concert_classic.jpg'
 
-import discordImage from '../images/discord.png'
+// import discordImage from '../images/discord.png'
 
 export const initialState = {
     // when in dev, change appURL to local url
